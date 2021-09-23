1431df4af432ed590d5a2c2b8d812adaec2b59fd
NetDwarf <netdwarf@users.noreply.github.com>
2021-09-23 16:59:51 +0200

    Update DOLDatebase.dll and GameServer.dll
    Delete custom DataQuestRewardQuest

--- DOLToolbox/Content/DatabaseManager.cs
+++ DOLToolbox/Content/DatabaseManager.cs
@@ -29,7 +29,6 @@ namespace DOLToolbox
             typeof(MobXLootTemplate),
             typeof(LootTemplate),
             typeof(WorldObject),
-            typeof(DBDQRewardQ)
                 
         };
 
